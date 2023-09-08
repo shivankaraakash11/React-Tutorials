9483837da818028900910e672888ec9cadcbbaea
shivankaraakash11 <shivankaraakash11@gmail.com>
2023-09-08 18:41:03 +0530

    full tutorials

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import Area
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import default import,{named import} from 'somelibrary';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+// let's create our first component
+
+function myFun(){
+ const x=10; // store data in variable. this is normal js function
+}
+
+// 1.RFC 
+function MyComponent(){   // Always use PascalCase for component name
+  // 1. state/variable ( state is nothing but a variable.)
+    const [salary, setsalary] = useState(2000)  // hooks variable
+
+  // 2. Function / Method
+  let incr =()=>{  // inner function
+    setsalary(salary + 1000)
+  }
+
+  // 3. Return Statement
+  return (
+        <div>
+          <button onClick={()=>{incr();}}>Increment</button>
+          <h2>My salary is {salary}</h2>
+        </div>
+        )
+}              
+
+
+const root = ReactDOM.createRoot(document.getElementById('root2'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello <MyComponent /></h1>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
